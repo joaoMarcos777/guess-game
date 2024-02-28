ea8b415aaf5f262ac9afa8f89ee0aef88acefb38
João Marcos <joaomarcos@frwk.com.br>
2024-02-28 16:33:14 -0300

    feat: add validations, refactoring

--- script.js
+++ script.js
@@ -1,46 +1,68 @@
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const inputNumber = document.querySelector("#inputNumber");
+
 let randomNumber = 0;
 let attempts = 1;
 
-const screen1 = document.querySelector(".screen1");
-const screen2 = document.querySelector(".screen2");
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+document.addEventListener("keyup", handleResetKey);
+inputNumber.addEventListener("keyup", validateInputNumber);
+
+generateRandomNumber();
 
 function generateRandomNumber() {
   randomNumber = Math.round(Math.random() * 10);
   return randomNumber;
 }
 
-generateRandomNumber();
-
 function handleTryClick(event) {
   event.preventDefault();
+  const number = Number(inputNumber.value);
 
-  const inputNumber = document.querySelector("#inputNumber");
+  if (number != "" && number != null && number != undefined) {
+    if (number == randomNumber) {
+      toggleScreen();
+      screen2.querySelector("h2").innerHTML = `Got it in ${attempts} attempts.`;
+    }
 
-  console.log(randomNumber);
-
-  if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide");
-    screen2.classList.remove("hide");
-
-    screen2.querySelector("h2").innerHTML = `Got it in ${attempts} attempts.`;
+    clearInputNumber();
+    attempts++;
   }
-
-  inputNumber.value = "";
-  attempts++;
 }
 
 function handleResetClick(event) {
   event.preventDefault();
 
-  screen1.classList.remove("hide");
-  screen2.classList.add("hide");
+  toggleScreen();
 
   generateRandomNumber();
   attempts = 1;
 }
 
-const btnTry = document.querySelector("#btnTry");
-const btnReset = document.querySelector("#btnReset");
+function handleResetKey(event) {
+  const containsHide = screen1.classList.contains("hide");
+  if (event.key == "Enter" && containsHide) {
+    handleResetClick(event);
+  }
+}
 
-btnTry.addEventListener("click", handleTryClick);
-btnReset.addEventListener("click", handleResetClick);
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
+
+function clearInputNumber() {
+  inputNumber.value = "";
+}
+
+function validateInputNumber() {
+  const number = Number(inputNumber.value);
+  if (number < 0 || number > 10) {
+    alert("Please enter a valid number.");
+    clearInputNumber();
+  }
+}
